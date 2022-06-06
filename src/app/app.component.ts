4b50885ae420afb5555214591f9364362e2d2cd5
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-06-06 17:55:15 -0500

    ngFor with arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,37 +11,72 @@ export class AppComponent {
   img = "https://source.unsplash.com/random";
   btnDisabled = true;
   person = {
-    name:"Saul",
+    name: "Saul",
     age: 25,
-    avatar : "https://source.unsplash.com/random"
+    avatar: "https://source.unsplash.com/random"
   }
 
-  names:string[] = ['Mike','Saul','Earvin'];
+  names: string[] = ['Mike', 'Saul', 'Earvin'];
   newName = '';
-  addName(){
+
+  products:Product[] =  [
+    {
+      name: 'The best toy',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Brand new bike',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Album',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'My books',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Dog house',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Glasses',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
+  addName() {
     this.names.push(this.newName);
     this.newName = '';
   }
 
-  public toggleButton(){
+  public toggleButton() {
     this.btnDisabled = !this.btnDisabled;
   }
 
-  increaseAge(){
+  increaseAge() {
     this.person.age += 1;
   }
 
-  onScroll(event: Event){
+  onScroll(event: Event) {
     const element = event.target as HTMLElement;
     console.log(element.scrollTop);
   }
 
-  changeName(event: Event){
+  changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
   }
 
-  deleteName(index:number){
-    this.names.splice(index,1);
+  deleteName(index: number) {
+    this.names.splice(index, 1);
   }
 }
